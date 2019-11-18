f1ff0ca6855b9c0c33919df803d4666ad363affa
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-11-18 23:21:20 +0100

    add sqlalchemy first config

--- application.py
+++ application.py
@@ -1,8 +1,25 @@
 from flask import Flask , render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 application = Flask(__name__)
+
+# Database initial config
+application.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
+db = SQLAlchemy(application)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return 
+
+
+
 @application.route("/")
 def index():
+    # pending to change to jsonify 
     return render_template("index.html")
 if __name__ == "__main__":
     application.debug = True
