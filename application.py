c810767de524297981af2fa3cf842f6070bf31dc
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-11-18 23:37:44 +0100

    add jsonify

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 application = Flask(__name__)
